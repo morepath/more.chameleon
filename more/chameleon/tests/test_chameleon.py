53611a240c6f0f4b3c356686fc5dac63df5390af
taschini <taschini@users.noreply.github.com>
2016-07-19 08:46:03 +0200

    Updated the test suite to work with Morepath 0.15.

--- more/chameleon/tests/test_chameleon.py
+++ more/chameleon/tests/test_chameleon.py
@@ -1,14 +1,8 @@
-import morepath
-import more.chameleon
 import os
 from more.chameleon.main import is_morepath_template_auto_reload
 from webtest import TestApp as Client
 from .fixtures import (
     template, template_macro, override_template, override_template_loader)
-
-
-def setup_module(module):
-    morepath.disable_implicit()
 
 
 def test_is_morepath_template_auto_reload():
@@ -39,9 +33,6 @@ def test_is_morepath_template_auto_reload():
 
 def test_template():
 
-    morepath.scan(more.chameleon, ignore=['.tests'])
-    morepath.scan(template)
-    morepath.commit(template.App)
     c = Client(template.App())
 
     response = c.get('/persons/world')
@@ -55,9 +46,6 @@ def test_template():
 
 def test_template_macro():
 
-    morepath.scan(more.chameleon, ignore=['.tests'])
-    morepath.scan(template_macro)
-    morepath.commit(template_macro.App)
     c = Client(template_macro.App())
 
     response = c.get('/persons/world')
@@ -75,9 +63,6 @@ def test_template_macro():
 
 
 def test_override_template():
-    morepath.scan(more.chameleon, ignore=['.tests'])
-    morepath.scan(override_template)
-    morepath.commit(override_template.App, override_template.SubApp)
     c = Client(override_template.App())
 
     response = c.get('/persons/world')
@@ -100,12 +85,6 @@ def test_override_template():
 
 
 def test_override_template_loader():
-    morepath.scan(more.chameleon, ignore=['.tests'])
-    morepath.scan(override_template_loader)
-    morepath.commit(
-        override_template_loader.App,
-        override_template_loader.SubApp
-    )
     c = Client(override_template_loader.App())
 
     response = c.get('/persons/world')
