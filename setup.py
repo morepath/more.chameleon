422d5886542d1fc36c1311750c12d228007d6988
Henri Hulski <henri.hulski@gazeta.pl>
2016-07-28 22:05:54 +0200

    Clean up meta data

--- setup.py
+++ setup.py
@@ -6,36 +6,42 @@ long_description = '\n'.join((
     io.open('CHANGES.txt', encoding='utf-8').read()
 ))
 
-setup(name='more.chameleon',
-      version='0.3.dev0',
-      description="Chameleon template integration for Morepath",
-      long_description=long_description,
-      author="Martijn Faassen",
-      author_email="faassen@startifact.com",
-      keywords='morepath chameleon',
-      license="BSD",
-      url="http://pypi.python.org/pypi/more.chameleon",
-      namespace_packages=['more'],
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      classifiers=[
-          "License :: OSI Approved :: BSD License",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-      ],
-      install_requires=[
+setup(
+    name='more.chameleon',
+    version='0.3.dev0',
+    description="Chameleon template integration for Morepath",
+    long_description=long_description,
+    author="Martijn Faassen",
+    author_email="faassen@startifact.com",
+    keywords='morepath chameleon',
+    license="BSD",
+    url="http://github.com/morepath/more.chameleon",
+    namespace_packages=['more'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Environment :: Web Environment",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        'Development Status :: 5 - Production/Stable'
+    ],
+    install_requires=[
         'setuptools',
         'morepath >= 0.15',
         'chameleon >= 2.20'
+    ],
+    extras_require=dict(
+        test=[
+            'pytest >= 2.6.0',
+            'pytest-cov',
+            'WebTest'
         ],
-      extras_require=dict(
-        test=['pytest >= 2.6.0',
-              'pytest-cov',
-              'WebTest'],
-        ),
-      )
+    ),
+)
