db48c595f3d6d49d193b479fc83ceb70f6e9be7f
Jürgen Gmach <juergen.gmach@googlemail.com>
2021-01-19 19:32:00 +0100

    use github actions for CI (#8)
    also add support for Python 3.9

--- more/chameleon/tests/test_chameleon.py
+++ more/chameleon/tests/test_chameleon.py
@@ -2,32 +2,36 @@ import os
 from more.chameleon.main import is_morepath_template_auto_reload
 from webtest import TestApp as Client
 from .fixtures import (
-    template, template_macro, override_template, override_template_loader)
+    template,
+    template_macro,
+    override_template,
+    override_template_loader,
+)
 
 
 def test_is_morepath_template_auto_reload():
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = ''
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = ""
     assert not is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = 'False'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "False"
     assert not is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = 'FALSE'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "FALSE"
     assert not is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = '0'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "0"
     assert not is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = '1'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "1"
     assert is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = 'on'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "on"
     assert is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = 'yes'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "yes"
     assert is_morepath_template_auto_reload()
 
-    os.environ['MOREPATH_TEMPLATE_AUTO_RELOAD'] = 'True'
+    os.environ["MOREPATH_TEMPLATE_AUTO_RELOAD"] = "True"
     assert is_morepath_template_auto_reload()
 
 
@@ -35,21 +39,26 @@ def test_template():
 
     c = Client(template.App())
 
-    response = c.get('/persons/world')
-    assert response.body == b'''\
+    response = c.get("/persons/world")
+    assert (
+        response.body
+        == b"""\
 <html>
 <body>
 <p>Hello world!</p>
 </body>
-</html>'''
+</html>"""
+    )
 
 
 def test_template_macro():
 
     c = Client(template_macro.App())
 
-    response = c.get('/persons/world')
-    assert response.body == b'''\
+    response = c.get("/persons/world")
+    assert (
+        response.body
+        == b"""\
 <html>
 <head>
 </head>
@@ -59,36 +68,45 @@ def test_template_macro():
 </div>
 </body>
 </html>
-'''
+"""
+    )
 
 
 def test_override_template():
     c = Client(override_template.App())
 
-    response = c.get('/persons/world')
-    assert response.body == b'''\
+    response = c.get("/persons/world")
+    assert (
+        response.body
+        == b"""\
 <html>
 <body>
 <p>Hello world!</p>
 </body>
-</html>'''
+</html>"""
+    )
 
     c = Client(override_template.SubApp())
 
-    response = c.get('/persons/world')
-    assert response.body == b'''\
+    response = c.get("/persons/world")
+    assert (
+        response.body
+        == b"""\
 <html>
 <body>
 <div>Hi world!</div>
 </body>
-</html>'''
+</html>"""
+    )
 
 
 def test_override_template_loader():
     c = Client(override_template_loader.App())
 
-    response = c.get('/persons/world')
-    assert response.body == b'''\
+    response = c.get("/persons/world")
+    assert (
+        response.body
+        == b"""\
 <html>
 <head>
 </head>
@@ -98,12 +116,15 @@ def test_override_template_loader():
 </div>
 </body>
 </html>
-'''
+"""
+    )
 
     c = Client(override_template_loader.SubApp())
 
-    response = c.get('/persons/world')
-    assert response.body == b'''\
+    response = c.get("/persons/world")
+    assert (
+        response.body
+        == b"""\
 <html>
 <head>
 </head>
@@ -113,4 +134,5 @@ def test_override_template_loader():
 </div>
 </body>
 </html>
-'''
+"""
+    )
